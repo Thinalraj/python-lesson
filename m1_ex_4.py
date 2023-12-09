ac8bf5850c356e5f4b7c5b4f8be7267b2069c475
Thinal Raj <thinal@live.com>
2023-12-09 17:15:44 +0800

    Create m1_ex_4.py

---
+++ m1_ex_4.py
@@ -0,0 +1,11 @@
+#coder: 
+#date:
+#lesson: 1
+#detail: Exercise 4
+
+print("Ramli Corner")
+print("date: 20/11/2023")
+print("order ID: 3289")
+print("table No: 10")
+print("Nasi Goreng Kambing x 1")
+print("your order will be served shortly")
