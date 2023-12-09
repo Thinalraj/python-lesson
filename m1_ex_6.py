7993f53ff0a02b93976f1453c37f99f375482cdc
Thinal Raj <thinal@live.com>
2023-12-09 17:20:54 +0800

    Create m1_ex_6.py

---
+++ m1_ex_6.py
@@ -0,0 +1,11 @@
+#coder:
+#date:
+#lesson:1
+#detail: Exercise 6
+
+print("Ramli Corner")
+print("date: 20/11/2023")
+print("order ID: 3289")
+print("Cashier: Ramli")
+print("Nasi Goreng Kambing RM 10.00")
+print("Total: RM 10.00")
