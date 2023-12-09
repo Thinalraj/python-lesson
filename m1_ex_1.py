1e86d0f5d5fab737b60ef98c2030dec1bcfed8dc
Thinal Raj <thinal@live.com>
2023-12-09 17:09:24 +0800

    Create m1_ex_1.py

---
+++ m1_ex_1.py
@@ -0,0 +1,9 @@
+#coder:
+#date:
+#lesson: 1
+#detail: Exercise 
+
+print("Welcome to Tasty Ramli Corner")
+print("Location: 123 Delicious Street, Foodie Town")
+print("Opening Hours: Monday to Saturday, 10 AM -8 PM")
+print("Come and join us for a culinary adventure!")
