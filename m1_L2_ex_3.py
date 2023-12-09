18be04612a479efeef411b217d6f17d78b4c8548
Thinal Raj <thinal@live.com>
2023-12-09 17:52:47 +0800

    Create m1_L2_ex_3.py

---
+++ m1_L2_ex_3.py
@@ -0,0 +1,10 @@
+
+
+name = input("Enter your name:")
+
+dining_option = input("Takeaway or dine-in ?")
+
+food = input("Enter your option:")
+
+print("Hi",name)
+print("Your order",food, "will served shortly" )
