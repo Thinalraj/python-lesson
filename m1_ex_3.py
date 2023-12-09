b95ae8895eaf94187b2f06d4b4fc6d64957b0157
Thinal Raj <thinal@live.com>
2023-12-09 17:13:26 +0800

    Create m1_ex_3.py

---
+++ m1_ex_3.py
@@ -0,0 +1,11 @@
+#coder: 
+#date:
+#lesson: 1
+#detail: Exercise 3
+
+print("Nasi Goreng Kambing Recipe:")
+print("1. Fresh meat from local.")
+print("2. Spice and herbs from India.")
+print("3. Aromatic basmati rice")
+print("4. Nutritious red palm oil.")
+print("5. Fresh vegetables from local farm.")
