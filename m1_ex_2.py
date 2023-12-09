7e17d7ad95271e4b748f775e71bb143bebe726df
Thinal Raj <thinal@live.com>
2023-12-09 17:11:32 +0800

    Create m1_ex_2.py

---
+++ m1_ex_2.py
@@ -0,0 +1,10 @@
+#coder:
+#date:
+#lesson: 1
+#detail: Exercise 2
+
+print("1. Nasi Goreng Ayam Special")
+print("2. Nasi Goreng Kambing Special")
+print("3. Nasi Goreng Kampung Special")
+print("4. Mee Goreng Mamak")
+print("5. Maggie Goreng Mamak")
